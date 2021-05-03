f630eb76fdb302c44db02215eeccdc393e897288
jonathan Haas <haasj@upenn.edu>
2021-05-03 15:33:47 -0400

    first commit

---
+++ global2.js
@@ -0,0 +1,235 @@
+// preload images used in menu and toolbar rollovers
+
+if (document.images)
+   {
+   // menu 
+
+office1 = new Image(175,20);
+   office1.src = "../images/nav/office-on.gif";
+   office2 = new Image(175,20);
+  office2.src = "../images/nav/office-off.gif";
+
+   taffairs1 = new Image(175,20);
+   taffairs1.src = "../images/nav/taffairs-on.gif";
+   taffairs2 = new Image(175,20);
+  taffairs2.src = "../images/nav/taffairs-off.gif";
+
+   trustees1 = new Image(175,20);
+   trustees1.src = "../images/nav/trustees-on.gif";
+   trustees2 = new Image(175,20);
+   trustees2.src = "../images/nav/trustees-off.gif";
+
+   statutes1 = new Image(175,20);
+   statutes1.src = "../images/nav/statutes-on.gif";
+   statutes2 = new Image(175,20);
+   statutes2.src = "../images/nav/statutes-off.gif";
+
+  
+   classes1 = new Image(175,20);
+   classes1.src = "../images/nav/classes-on.gif";
+   classes2 = new Image(175,20);
+   classes2.src = "../images/nav/classes-off.gif";
+
+   overaffairs1 = new Image(175,20);
+   overaffairs1.src = "../images/nav/overaffairs-on.gif";
+   overaffairs2 = new Image(175,20);
+   overaffairs2.src = "../images/nav/overaffairs-off.gif";
+
+   boards1 = new Image(175,20);
+   boards1.src = "../images/nav/boards-on.gif";
+   boards2 = new Image(175,20);
+   boards2.src = "../images/nav/boards-off.gif";
+   
+    diplomas1 = new Image(175,20);
+   diplomas1.src = "../images/nav/diplomas-on.gif";
+   diplomas2 = new Image(175,20);
+   diplomas2.src = "../images/nav/diplomas-off.gif";
+
+   use1 = new Image(175,20);
+   use1.src = "../images/nav/use-on.gif";
+   use2 = new Image(175,20);
+   use2.src = "../images/nav/use-off.gif";
+
+   staff1 = new Image(175,20);
+   staff1.src = "../images/nav/staff-on.gif";
+   staff2 = new Image(175,20);
+   staff2.src = "../images/nav/staff-off.gif";
+
+   ucouncil1 = new Image(175,20);
+   ucouncil1.src = "../images/nav/ucouncil-on.gif";
+   ucouncil2 = new Image(175,20);
+   ucouncil2.src = "../images/nav/ucouncil-off.gif";
+   
+    commence1 = new Image(175,20);
+   commence1.src = "../images/nav/commence-on.gif";
+   commence2 = new Image(175,20);
+   commence2.src = "../images/nav/commence-off.gif";
+
+   bylaws1 = new Image(175,20);
+   bylaws1.src = "../images/nav/bylaws-on.gif";
+   bylaws2 = new Image(175,20);
+   bylaws2.src = "../images/nav/bylaws-off.gif";
+
+   steering1 = new Image(175,20);
+   steering1.src = "../images/nav/steering-on.gif";
+   steering2 = new Image(175,20);
+   steering2.src = "../images/nav/steering-off.gif";
+   
+    committees1 = new Image(175,20);
+   committees1.src = "../images/nav/committees-on.gif";
+   committees2 = new Image(175,20);
+   committees2.src = "../images/nav/committees-off.gif";
+
+   forum1 = new Image(175,20);
+   forum1.src = "../images/nav/forum-on.gif";
+   forum2 = new Image(175,20);
+   forum2.src = "../images/nav/forum-off.gif";
+
+   membership1 = new Image(175,20);
+   membership1.src = "../images/nav/membership-on.gif";
+   membership2 = new Image(175,20);
+   membership2.src = "../images/nav/membership-off.gif";
+
+overview1 = new Image(175,20);
+   overview1.src = "../images/nav/overview-on.gif";
+ overview2 = new Image(175,20);
+  overview2.src = "../images/nav/overview-off.gif";
+
+   ceremony1 = new Image(175,20);
+   ceremony1.src = "../images/nav/ceremony-on.gif";
+   ceremony2 = new Image(175,20);
+  ceremony2.src = "../images/nav/ceremony-off.gif";
+
+program1 = new Image(175,20);
+   program1.src = "../images/nav/program-on.gif";
+   program2 = new Image(175,20);
+   program2.src = "../images/nav/program-off.gif";
+
+   procession1 = new Image(175,20);
+   procession1.src = "../images/nav/procession-on.gif";
+   procession2 = new Image(175,20);
+   procession2.src = "../images/nav/procession-off.gif";
+
+   tickets1 = new Image(175,20);
+   tickets1.src = "../images/nav/tickets-on.gif";
+   tickets2 = new Image(175,20);
+   tickets2.src = "../images/nav/tickets-off.gif";
+
+   webcast1 = new Image(175,20);
+   webcast1.src = "../images/nav/webcast-on.gif";
+   webcast2 = new Image(175,20);
+   webcast2.src = "../images/nav/webcast-off.gif";
+
+   symposia1 = new Image(175,20);
+   symposia1.src = "../images/nav/symposia-on.gif";
+   symposia2 = new Image(175,20);
+   symposia2.src = "../images/nav/symposia-off.gif";
+ 
+    speakers1 = new Image(175,20);
+   speakers1.src = "../images/nav/speakers-on.gif";
+   speakers2 = new Image(175,20);
+   speakers2.src = "../images/nav/speakers-off.gif";
+
+   travel1 = new Image(175,20);
+   travel1.src = "../images/nav/travel-on.gif";
+   travel2 = new Image(175,20);
+   travel2.src = "../images/nav/travel-off.gif";
+
+  traveling1 = new Image(175,20);
+   traveling1.src = "../images/nav/traveling-on.gif";
+   traveling2 = new Image(175,20);
+   traveling2.src = "../images/nav/traveling-off.gif";
+
+reception1 = new Image(175,20);
+   reception1.src = "../images/nav/reception-on.gif";
+   reception2 = new Image(175,20);
+   reception2.src = "../images/nav/reception-off.gif";
+
+concert1 = new Image(175,20);
+   concert1.src = "../images/nav/concert-on.gif";
+   concert2 = new Image(175,20);
+   concert2.src = "../images/nav/concert-off.gif";
+
+community1 = new Image(175,20);
+   community1.src = "../images/nav/community-on.gif";
+   community2 = new Image(175,20);
+   community2.src = "../images/nav/community-off.gif";
+
+ past1 = new Image(175,20);
+   past1.src = "../images/nav/past-on.gif";
+   past2 = new Image(175,20);
+    past2.src = "../images/nav/past-off.gif";
+   
+   parking1 = new Image(175,20);
+   parking1.src = "../images/nav/parking-on.gif";
+   parking2 = new Image(175,20);
+   parking2.src = "../images/nav/parking-off.gif";
+   
+    accommodations1 = new Image(175,20);
+   accommodations1.src = "../images/nav/accommodations-on.gif";
+   accommodations2 = new Image(175,20);
+   accommodations2.src = "../images/nav/accommodations-off.gif";
+   
+   contact1 = new Image(175,20);
+   contact1.src = "../images/nav/contact-on.gif";
+   contact2 = new Image(175,20);
+   contact2.src = "../images/nav/contact-off.gif";
+   
+    inaug1 = new Image(175,20);
+   inaug1.src = "../images/nav/inaug-on.gif";
+  inaug2 = new Image(175,20);
+   inaug2.src = "../images/nav/inaug-off.gif";
+   
+    press1 = new Image(175,20);
+    press1.src = "../images/nav/press-on.gif";
+   press2 = new Image(175,20);
+    press2.src = "../images/nav/press-off.gif";
+  
+
+   // toolbar
+
+   home1 = new Image(28,23);
+   home1.src = "../images/toolbar/home1.gif";
+   home2 = new Image(28,23);
+   home2.src = "../images/toolbar/home2.gif";
+
+   az1 = new Image(44,23);
+   az1.src = "../images/toolbar/az1.gif";
+   az2 = new Image(44,23);
+   az2.src = "../images/toolbar/az2.gif";
+
+   directories1 = new Image(54,23);
+   directories1.src = "../images/toolbar/directories1.gif";
+   directories2 = new Image(54,23);
+   directories2.src = "../images/toolbar/directories2.gif";
+
+   calendar1 = new Image(45,23);
+   calendar1.src = "../images/toolbar/calendar1.gif";
+   calendar2 = new Image(45,23);
+   calendar2.src = "../images/toolbar/calendar2.gif";
+
+   maps1 = new Image(27,23);
+   maps1.src = "../images/toolbar/maps1.gif";
+   maps2 = new Image(27,23);
+   maps2.src = "../images/toolbar/maps2.gif";
+
+   search1 = new Image(33,23);
+   search1.src = "../images/toolbar/search1.gif";
+   search2 = new Image(33,23);
+   search2.src = "../images/toolbar/search2.gif";
+
+   advanced1 = new Image(72,23);
+   advanced1.src = "../images/toolbar/advanced1a.gif";
+   advanced2 = new Image(72,23);
+   advanced2.src = "../images/toolbar/advanced2a.gif";
+   }
+
+// simple image replacement
+
+function imgSwap(imgDocID,imgObjName)
+   {
+   if (document.images)
+      {
+      document.images[imgDocID].src = eval(imgObjName + ".src")
+      }
+   }
---
+++ global3.js
@@ -0,0 +1,234 @@
+// preload images used in menu and toolbar rollovers
+
+if (document.images)
+   {
+   // menu 
+
+office1 = new Image(175,20);
+   office1.src = "/secretary/images/nav/office-on.gif";
+   office2 = new Image(175,20);
+  office2.src = "/secretary/images/nav/office-off.gif";
+
+   taffairs1 = new Image(175,20);
+   taffairs1.src = "/secretary/images/nav/taffairs-on.gif";
+   taffairs2 = new Image(175,20);
+  taffairs2.src = "/secretary/images/nav/taffairs-off.gif";
+
+   trustees1 = new Image(175,20);
+   trustees1.src = "/secretary/images/nav/trustees-on.gif";
+   trustees2 = new Image(175,20);
+   trustees2.src = "/secretary/images/nav/trustees-off.gif";
+
+   statutes1 = new Image(175,20);
+   statutes1.src = "/secretary/images/nav/statutes-on.gif";
+   statutes2 = new Image(175,20);
+   statutes2.src = "/secretary/images/nav/statutes-off.gif";
+
+  
+   classes1 = new Image(175,20);
+   classes1.src = "/secretary/images/nav/classes-on.gif";
+   classes2 = new Image(175,20);
+   classes2.src = "/secretary/images/nav/classes-off.gif";
+
+   overaffairs1 = new Image(175,20);
+   overaffairs1.src = "/secretary/images/nav/overaffairs-on.gif";
+   overaffairs2 = new Image(175,20);
+   overaffairs2.src = "/secretary/images/nav/overaffairs-off.gif";
+
+   boards1 = new Image(175,20);
+   boards1.src = "/secretary/images/nav/boards-on.gif";
+   boards2 = new Image(175,20);
+   boards2.src = "/secretary/images/nav/boards-off.gif";
+   
+    diplomas1 = new Image(175,20);
+   diplomas1.src = "/secretary/images/nav/diplomas-on.gif";
+   diplomas2 = new Image(175,20);
+   diplomas2.src = "/secretary/images/nav/diplomas-off.gif";
+
+   use1 = new Image(175,20);
+   use1.src = "/secretary/images/nav/use-on.gif";
+   use2 = new Image(175,20);
+   use2.src = "/secretary/images/nav/use-off.gif";
+
+   staff1 = new Image(175,20);
+   staff1.src = "/secretary/images/nav/staff-on.gif";
+   staff2 = new Image(175,20);
+   staff2.src = "/secretary/images/nav/staff-off.gif";
+
+   ucouncil1 = new Image(175,20);
+   ucouncil1.src = "/secretary/images/nav/ucouncil-on.gif";
+   ucouncil2 = new Image(175,20);
+   ucouncil2.src = "/secretary/images/nav/ucouncil-off.gif";
+   
+    commence1 = new Image(175,20);
+   commence1.src = "/secretary/images/nav/commence-on.gif";
+   commence2 = new Image(175,20);
+   commence2.src = "/secretary/images/nav/commence-off.gif";
+
+   bylaws1 = new Image(175,20);
+   bylaws1.src = "/secretary/images/nav/bylaws-on.gif";
+   bylaws2 = new Image(175,20);
+   bylaws2.src = "/secretary/images/nav/bylaws-off.gif";
+
+   steering1 = new Image(175,20);
+   steering1.src = "/secretary/images/nav/steering-on.gif";
+   steering2 = new Image(175,20);
+   steering2.src = "/secretary/images/nav/steering-off.gif";
+   
+    committees1 = new Image(175,20);
+   committees1.src = "/secretary/images/nav/committees-on.gif";
+   committees2 = new Image(175,20);
+   committees2.src = "/secretary/images/nav/committees-off.gif";
+
+   forum1 = new Image(175,20);
+   forum1.src = "/secretary/images/nav/forum-on.gif";
+   forum2 = new Image(175,20);
+   forum2.src = "/secretary/images/nav/forum-off.gif";
+
+   membership1 = new Image(175,20);
+   membership1.src = "/secretary/images/nav/membership-on.gif";
+   membership2 = new Image(175,20);
+   membership2.src = "/secretary/images/nav/membership-off.gif";
+
+overview1 = new Image(175,20);
+   overview1.src = "/secretary/images/nav/overview-on.gif";
+ overview2 = new Image(175,20);
+  overview2.src = "/secretary/images/nav/overview-off.gif";
+
+   ceremony1 = new Image(175,20);
+   ceremony1.src = "/secretary/images/nav/ceremony-on.gif";
+   ceremony2 = new Image(175,20);
+  ceremony2.src = "/secretary/images/nav/ceremony-off.gif";
+
+program1 = new Image(175,20);
+   program1.src = "/secretary/images/nav/program-on.gif";
+   program2 = new Image(175,20);
+   program2.src = "/secretary/images/nav/program-off.gif";
+
+   procession1 = new Image(175,20);
+   procession1.src = "/secretary/images/nav/procession-on.gif";
+   procession2 = new Image(175,20);
+   procession2.src = "/secretary/images/nav/procession-off.gif";
+
+   tickets1 = new Image(175,20);
+   tickets1.src = "/secretary/images/nav/tickets-on.gif";
+   tickets2 = new Image(175,20);
+   tickets2.src = "/secretary/images/nav/tickets-off.gif";
+
+   webcast1 = new Image(175,20);
+   webcast1.src = "/secretary/images/nav/webcast-on.gif";
+   webcast2 = new Image(175,20);
+   webcast2.src = "/secretary/images/nav/webcast-off.gif";
+
+   symposia1 = new Image(175,20);
+   symposia1.src = "/secretary/images/nav/symposia-on.gif";
+   symposia2 = new Image(175,20);
+   symposia2.src = "/secretary/images/nav/symposia-off.gif";
+ 
+    speakers1 = new Image(175,20);
+  speakers1.src = "/secretary/images/nav/speakers-on.gif";
+   speakers2 = new Image(175,20);
+   speakers2.src = "/secretary/images/nav/speakers-off.gif";
+
+   travel1 = new Image(175,20);
+   travel1.src = "/secretary/images/nav/travel-on.gif";
+   travel2 = new Image(175,20);
+   travel2.src = "/secretary/images/nav/travel-off.gif";
+
+reception1 = new Image(175,20);
+   reception1.src = "/secretary/images/nav/reception-on.gif";
+   reception2 = new Image(175,20);
+   reception2.src = "/secretary/images/nav/reception-off.gif";
+
+concert1 = new Image(175,20);
+   concert1.src = "/secretary/images/nav/concert-on.gif";
+   concert2 = new Image(175,20);
+   concert2.src = "/secretary/images/nav/concert-off.gif";
+
+community1 = new Image(175,20);
+   community1.src = "/secretary/images/nav/community-on.gif";
+   community2 = new Image(175,20);
+   community2.src = "/secretary/images/nav/community-off.gif";
+
+  traveling1 = new Image(175,20);
+   traveling1.src = "/secretary/images/nav/traveling-on.gif";
+   traveling2 = new Image(175,20);
+   traveling2.src = "/secretary/images/nav/traveling-off.gif";
+
+ past1 = new Image(175,20);
+   past1.src = "/secretary/images/nav/past-on.gif";
+   past2 = new Image(175,20);
+    past2.src = "/secretary/images/nav/past-off.gif";
+   
+   parking1 = new Image(175,20);
+   parking1.src = "/secretary/images/nav/parking-on.gif";
+   parking2 = new Image(175,20);
+   parking2.src = "/secretary/images/nav/parking-off.gif";
+   
+    accommodations1 = new Image(175,20);
+   accommodations1.src = "/secretary/images/nav/accommodations-on.gif";
+   accommodations2 = new Image(175,20);
+   accommodations2.src = "/secretary/images/nav/accommodations-off.gif";
+   
+   contact1 = new Image(175,20);
+   contact1.src = "/secretary/images/nav/contact-on.gif";
+   contact2 = new Image(175,20);
+   contact2.src = "/secretary/images/nav/contact-off.gif";
+   
+    inaug1 = new Image(175,20);
+   inaug1.src = "/secretary/images/nav/inaug-on.gif";
+  inaug2 = new Image(175,20);
+   inaug2.src = "/secretary/images/nav/inaug-off.gif";
+  
+    press1 = new Image(175,20);
+    press1.src = "/secretary/images/nav/press-on.gif";
+   press2 = new Image(175,20);
+    press2.src = "/secretary/images/nav/press-off.gif";
+
+   // toolbar
+
+   home1 = new Image(28,23);
+   home1.src = "/secretary/images/toolbar/home1.gif";
+   home2 = new Image(28,23);
+   home2.src = "/secretary/images/toolbar/home2.gif";
+
+   az1 = new Image(44,23);
+   az1.src = "/secretary/images/toolbar/az1.gif";
+   az2 = new Image(44,23);
+   az2.src = "/secretary/images/toolbar/az2.gif";
+
+   directories1 = new Image(54,23);
+   directories1.src = "/secretary/images/toolbar/directories1.gif";
+   directories2 = new Image(54,23);
+   directories2.src = "/secretary/images/toolbar/directories2.gif";
+
+   calendar1 = new Image(45,23);
+   calendar1.src = "/secretary/images/toolbar/calendar1.gif";
+   calendar2 = new Image(45,23);
+   calendar2.src = "/secretary/images/toolbar/calendar2.gif";
+
+   maps1 = new Image(27,23);
+   maps1.src = "/secretary/images/toolbar/maps1.gif";
+   maps2 = new Image(27,23);
+   maps2.src = "/secretary/images/toolbar/maps2.gif";
+
+   search1 = new Image(33,23);
+   search1.src = "/secretary/images/toolbar/search1.gif";
+   search2 = new Image(33,23);
+   search2.src = "/secretary/images/toolbar/search2.gif";
+
+   advanced1 = new Image(72,23);
+   advanced1.src = "/secretary/images/toolbar/advanced1a.gif";
+   advanced2 = new Image(72,23);
+   advanced2.src = "/secretary/images/toolbar/advanced2a.gif";
+   }
+
+// simple image replacement
+
+function imgSwap(imgDocID,imgObjName)
+   {
+   if (document.images)
+      {
+      document.images[imgDocID].src = eval(imgObjName + ".src")
+      }
+   }
